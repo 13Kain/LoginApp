9cd0cfc6daf5b2287222fbd0b532ef857c246175
Nikita Yashin <n.yashin88@icloud.com>
2022-03-09 11:41:48 +0300

    finish line

--- LoginApp/GreetingsViewController.swift
+++ LoginApp/GreetingsViewController.swift
@@ -12,9 +12,23 @@ class GreetingsViewController: UIViewController {
     @IBOutlet var logoutButton: UIButton!
     @IBOutlet var userIDLabel: UILabel!
     
+    var userID: String!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         navigationItem.hidesBackButton = true
+        userIDLabel.text = userID
+        
+        /*let gradientLayer = CAGradientLayer()
+        gradientLayer.frame = view.bounds
+        gradientLayer.colors  = [
+            UIColor.systemPink.cgColor,
+            UIColor.systemOrange.cgColor
+        ]
+        view.layer.addSublayer(gradientLayer)
+        */
     }
-
+    @IBAction func logOutButtonPressed() {
+        dismiss(animated: true)
+    }
 }
--- LoginApp/ViewController.swift
+++ LoginApp/ViewController.swift
@@ -12,12 +12,13 @@ class ViewController: UIViewController {
     @IBOutlet var forgotPswrdButton: UIButton!
     @IBOutlet var forgotLoginButton: UIButton!
     
+    @IBOutlet var passwordUserField: UITextField!
+    @IBOutlet var loginUserField: UITextField!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
 
     @IBAction func forgotLoginButtonPressed() {
         showAlert(title: "Hack me", message: "My login is admin")
@@ -27,6 +28,16 @@ class ViewController: UIViewController {
         showAlert(title: "Hack me", message: "My password is admin")
     }
     
+    @IBAction func loginButtonPressed() {
+        if passwordUserField.text != "admin", loginUserField.text != "admin" {
+                showAlert(title: "Invalid login or password", message: "Please try again")
+        }
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let greetingsVC = segue.destination as? GreetingsViewController else { return }
+        greetingsVC.userID = loginUserField.text
+    }
     
     private func showAlert(title: String, message: String) {
         let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
